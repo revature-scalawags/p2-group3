137185538109c2d94602886556197e37ad04aaf3
Myung Jin Song <jsong220@gmail.com>
2021-01-17 23:49:59 -0800

    Counting hashtags fixed

--- compileScript.sh
+++ compileScript.sh
@@ -3,4 +3,4 @@ set -e
 
 sbt package
 docker cp target/scala-2.12/twitter-hashtags_2.12-1.0.jar spark-master:/tmp/twitter-hashtags.jar
-docker exec spark-master bash -c "./spark/bin/spark-submit --class "Main" --master local[*] /tmp/twitter-hashtags.jar"
+docker exec spark-master bash -c "./spark/bin/spark-submit --class "Main" --master local[2] /tmp/twitter-hashtags.jar"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,7 @@ object Main {
     val spark = SparkSession
       .builder()
       .appName("count hashtags")
-      .master("local[*]")
+      .master("local[2]")
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
     countHashtags(spark)
@@ -16,22 +16,70 @@ object Main {
     def countHashtags(spark: SparkSession) {
       import spark.implicits._
       // val jsonfile = spark.read.option("multiline", "true").json("/datalake/00.json")
-      val jsonfile = spark.read.json("/datalake").cache()
+      val jsonfile = spark.read.json("/datalake/*/*bz2").cache()
       // jsonfile.show()
       // jsonfile.printSchema()
-      jsonfile
-        .filter(jsonfile("entities.hashtags.text").isNotNull)
-        .groupBy("entities.hashtags.text")
+
+      //works cited: https://stackoverflow.com/questions/39255973/split-1-column-into-3-columns-in-spark-scala
+      //prints out total count of trump hashtags
+      val countTrump: Unit = jsonfile
+        .withColumn("_tmp", split($"entities.hashtags.text".getItem(0), "\\,"))
+        .select($"_tmp".getItem(0).as("col1"))
+        .groupBy(("col1"))
+        .count()
+        .filter(lower($"col1") === "trump" || lower($"col1") === "donald")
+        .agg(sum($"count"))
+        .show()
+
+      //prints out count of hillary hashtags of all casing
+      val countClinton = jsonfile
+        .withColumn("_tmp", split($"entities.hashtags.text".getItem(0), "\\,"))
+        .select($"_tmp".getItem(0).as("col1"))
+        .groupBy(("col1"))
         .count()
         .sort($"count".desc)
-        .show(100, false)
-
-      //TODO:parse out each wrappedarray word
-      //random fails..
-      // val hashTags = jsonfile.select("entities.hashtags.text").collect().flatMap(_.getAs[mutable.WrappedArray[String]](0))
-      // hashTags.foreach(println)
-      //https://stackoverflow.com/questions/57346978/spark-split-is-not-a-member-of-org-apache-spark-sql-row
+        .filter(lower($"col1") === "hillary" || lower($"col1") === "clinton")
+        .show()
 
     }
   }
 }
+
+// case class mySchema(
+//   contributors: String,
+//   coordinates: String,
+//   created_at: String,
+//   delete: String,
+//   display_text_range: String,
+//   entities: Array[String],
+//   extended_entities: Array[String],
+//   extended_tweet: String,
+//   favorite_count: Int,
+//   favorited: Boolean,
+//   filter_level: String,
+//   geo: String,
+//   id: BigInt,
+//   id_str: BigInt,
+//   in_reply_to_screen_name: String,
+//   in_reply_to_status_id: BigInt,
+//   in_reply_to_status_id_str: BigInt,
+//   in_reply_to_user_id: BigInt,
+//   in_reply_to_user_id_str: BigInt,
+//   is_quote_status: Boolean,
+//   lang: String,
+//   place: String,
+//   possibly_sensitive: Boolean,
+//   quote_count: Int,
+//   quoted_status: String,
+//   quoted_status_id: Int,
+//   quoted_status_id_str: BigInt,
+//   quoted_status_permalink: Array[String],
+//   reply_count: Int,
+//   retweet_count: Int,
+//   retweeted: Boolean,
+//   retweeted_status: Array[String],
+//   source: String,
+//   text: String,
+//   timestamp_ms: BigInt,
+//   truncated: Boolean,
+//   user: Array[String])
